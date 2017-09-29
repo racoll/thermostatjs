ebe6258142feedfaea8da99639c8de2d22a1676e
robincollins <racollins1@me.com>
2017-09-29 15:58:53 +0100

    temp up down, reset, psm on and off

--- interface.js
+++ interface.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature);
+  updateTemperature();
 });
+
+$('#temperature-up').on('click', function() { // event listener
+  thermostat.up(); // update model
+  updateTemperature(); // update view
+});
+
+$('#temperature-down').click(function() { // this is an alternate version of .on('click'), with a sprinkle of jQuery syntactic sugar
+  thermostat.down();
+  updateTemperature();
+});
+
+$("#temperature-reset").click(function() {
+  thermostat.resetTemperature();
+  updateTemperature();
+});
+
+$("#powersaving-on").click(function() {
+  thermostat.switchPowerSavingModeOn();
+  $("#power-saving").text("on");
+  updateTemperature();
+});
+
+$("#powersaving-off").click(function() {
+  thermostat.switchPowerSavingModeOff();
+  $("#power-saving").text("off");
+  updateTemperature();
+});
+
+function updateTemperature() {
+  $("#temperature").text(thermostat.temperature);
+}
