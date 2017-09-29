3b4be8d0683c9ef817605d030be93b9c2f868658
robincollins <racollins1@me.com>
2017-09-29 15:01:32 +0100

    comlpete program

--- jasmine-standalone-2.8.0 (2)/src/thermostat.js
+++ jasmine-standalone-2.8.0 (2)/src/thermostat.js
@@ -1,5 +1,76 @@
 "use strict";
 
-function Thermostat() {
-  this.temperature = 20;
+function Thermostat() {  // created a thermostat object constructor
+  this.MINIMUM_TEMPERATURE = 10;
+  this.MAXIMUM_TEMPERATURE = 25;
+  this.DEFAULT_TEMPERATURE = 20; // and given it a property of temperature
+  this.temperature = this.DEFAULT_TEMPERATURE;
+  this.powerSavingMode = true;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 }
+
+Thermostat.prototype.getCurrentTemperature = function() { //then use prototype keyword to associate the
+  return this.temperature; // the getCurrentTemperature function to our Thermostat.
+};
+
+Thermostat.prototype.up = function() {
+  this.temperature += 1;
+  // console.log(this.temperature)
+};
+
+Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
+  this.temperature -= 1;
+};
+
+Thermostat.prototype.up = function() {
+  if (this.isMaximumTemperature()) {
+    return;
+  }
+  this.temperature += 1;
+};
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
+};
+
+Thermostat.prototype.isMaximumTemperature = function() {
+  return this.temperature === this.MAXIMUM_TEMPERATURE;
+};
+
+Thermostat.prototype.isPowerSavingModeOn = function() {
+  return this.powerSavingMode === true;
+};
+
+Thermostat.prototype.switchPowerSavingModeOff = function() {
+  this.powerSavingMode = false;
+};
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
+  this.powerSavingMode = true;
+};
+
+Thermostat.prototype.isMaximumTemperature = function() {
+  if (this.isPowerSavingModeOn() === false) {
+    return this.temperature === this.MAX_LIMIT_PSM_OFF;
+  }
+  return this.temperature === this.MAX_LIMIT_PSM_ON;
+};
+
+Thermostat.prototype.resetTemperature = function() {
+  this.temperature = this.DEFAULT_TEMPERATURE;
+};
+
+Thermostat.prototype.energyUsage = function() {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return "low-usage";
+  }
+  if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+    return "medium-usage";
+  }
+  return "high-usage";
+};
